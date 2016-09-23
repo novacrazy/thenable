2af97ae91f872f0f66315fc84b31c8cff8bfafcf
novacrazy <novacrazy@gmail.com>
2016-09-23 04:56:46 -0500

    Fix make_promise2 bug and flesh out to_thenable a bit more.

--- include/thenable/future.hpp
+++ include/thenable/future.hpp
@@ -79,7 +79,55 @@ namespace thenable {
     template <typename T, template <typename> typename FutureType>
     constexpr thenable_equivalent_t<FutureType<T>> to_thenable( FutureType<T> & );
 
+    /*
+     * The first two parameters of the following must be specified so as to not conflict with the above overloads.
+     * */
+
+    template <typename T0,
+              typename T1,
+              typename... Ts,
+        template <typename> typename FutureType0,
+        template <typename> typename FutureType1,
+        template <typename> typename... FutureTypes>
+    constexpr std::tuple<thenable_equivalent_t<FutureType0<T0>>,
+                         thenable_equivalent_t<FutureType1<T1>>,
+                         thenable_equivalent_t<FutureTypes<Ts>>...>
+    to_thenable( FutureType0<T0> &&fut0,
+                 FutureType1<T1> &&fut1,
+                 FutureTypes<Ts> &&... futures );
+
+    template <typename T0,
+              typename T1,
+              typename... Ts,
+        template <typename> typename FutureType0,
+        template <typename> typename FutureType1,
+        template <typename> typename... FutureTypes>
+    constexpr std::tuple<thenable_equivalent_t<FutureType0<T0>>,
+                         thenable_equivalent_t<FutureType1<T1>>,
+                         thenable_equivalent_t<FutureTypes<Ts>>...>
+    to_thenable( FutureType0<T0> &fut0,
+                 FutureType1<T1> &fut1,
+                 FutureTypes<Ts> &... futures );
+
+    /*
+     * Even though the compiler is smart enough to usually select the correct one, my IDE can't and I feel that means having
+     * the following also be named to_thenable would lead to real ambiguities someplace. For example,
+     * FutureType<T> could be std::tuple<int> and it would be ambiguous.
+     *
+     * So to solve that and clarify things, I'm just naming these to_thenable_tuple to differentiate them.
+     * */
+
+    template <typename... Ts, template <typename> typename... FutureTypes>
+    constexpr std::tuple<thenable_equivalent_t<FutureTypes<Ts>>...> to_thenable_tuple( std::tuple<FutureTypes<Ts>...> &&futures );
+
+    template <typename... Ts, template <typename> typename... FutureTypes>
+    constexpr std::tuple<thenable_equivalent_t<FutureTypes<Ts>>...> to_thenable_tuple( std::tuple<FutureTypes<Ts>...> &futures );
+
     //////////
+
+    /*
+     * then2 is a variation of then that returns a ThenableFuture instead of std::future
+     * */
 
     template <typename T, typename Functor, template <typename> typename FutureType, typename PolicyType = std::launch>
     inline ThenableFuture<result_of_cb_t<Functor, T>> then2( FutureType<T> &&fut, Functor &&fn, PolicyType policy = default_policy ) {
@@ -195,16 +243,70 @@ namespace thenable {
     //////////
 
     template <typename T, template <typename> typename FutureType>
-    constexpr thenable_equivalent_t<FutureType<T>> to_thenable( FutureType<T> &&fut ) {
+    constexpr thenable_equivalent_t<FutureType<T>>
+    to_thenable( FutureType<T> &&fut ) {
         return thenable_equivalent_t<FutureType<T>>( std::forward<FutureType<T>>( fut ));
     }
 
     template <typename T, template <typename> typename FutureType>
-    constexpr thenable_equivalent_t<FutureType<T>> to_thenable( FutureType<T> &fut ) {
+    constexpr thenable_equivalent_t<FutureType<T>>
+    to_thenable( FutureType<T> &fut ) {
         return thenable_equivalent_t<FutureType<T>>( std::move( fut ));
     }
 
+    template <typename T0,
+              typename T1,
+              typename... Ts,
+        template <typename> typename FutureType0,
+        template <typename> typename FutureType1,
+        template <typename> typename... FutureTypes>
+    constexpr std::tuple<thenable_equivalent_t<FutureType0<T0>>,
+                         thenable_equivalent_t<FutureType1<T1>>,
+                         thenable_equivalent_t<FutureTypes<Ts>>...>
+    to_thenable( FutureType0<T0> &&fut0,
+                 FutureType1<T1> &&fut1,
+                 FutureTypes<Ts> &&... futures ) {
+        return std::tuple<thenable_equivalent_t<FutureType0<T0>>,
+                          thenable_equivalent_t<FutureType1<T1>>,
+                          thenable_equivalent_t<FutureTypes<Ts>>...>( std::forward<FutureType0<T0>>( fut0 ),
+                                                                      std::forward<FutureType1<T1>>( fut1 ),
+                                                                      std::forward<FutureTypes<Ts>>( futures )... );
+    };
+
+    template <typename T0,
+              typename T1,
+              typename... Ts,
+        template <typename> typename FutureType0,
+        template <typename> typename FutureType1,
+        template <typename> typename... FutureTypes>
+    constexpr std::tuple<thenable_equivalent_t<FutureType0<T0>>,
+                         thenable_equivalent_t<FutureType1<T1>>,
+                         thenable_equivalent_t<FutureTypes<Ts>>...>
+    to_thenable( FutureType0<T0> &fut0,
+                 FutureType1<T1> &fut1,
+                 FutureTypes<Ts> &... futures ) {
+        return std::tuple<thenable_equivalent_t<FutureType0<T0>>,
+                          thenable_equivalent_t<FutureType1<T1>>,
+                          thenable_equivalent_t<FutureTypes<Ts>>...>( std::move( fut0 ),
+                                                                      std::move( fut1 ),
+                                                                      std::move( futures )... );
+    };
+
+    template <typename... Ts, template <typename> typename... FutureTypes>
+    constexpr std::tuple<thenable_equivalent_t<FutureTypes<Ts>>...> to_thenable_tuple( std::tuple<FutureTypes<Ts>...> &&futures ) {
+        return std::tuple<thenable_equivalent_t<FutureTypes<Ts>>...>( std::forward<std::tuple<FutureTypes<Ts>...>>( futures ));
+    };
+
+    template <typename... Ts, template <typename> typename... FutureTypes>
+    constexpr std::tuple<thenable_equivalent_t<FutureTypes<Ts>>...> to_thenable_tuple( std::tuple<FutureTypes<Ts>...> &futures ) {
+        return std::tuple<thenable_equivalent_t<FutureTypes<Ts>>...>( std::move( futures ));
+    };
+
     //////////
+
+    /*
+     * defer2 is a variation of defer that returns a ThenableFuture instead of std::future
+     * */
 
     template <typename Functor, typename... Args>
     inline ThenableFuture<typename std::result_of<Functor( Args... )>::type> defer2( Functor &&fn, Args &&... args ) {
@@ -213,12 +315,20 @@ namespace thenable {
 
     //////////
 
+    /*
+     * make_promise2 is a variation of make_promise that returns a ThenableFuture instead of std::future
+     * */
+
     template <typename T = void, typename Functor, typename PolicyType = std::launch>
     inline ThenableFuture<T> make_promise2( Functor &&fn, PolicyType policy = default_policy ) {
-        return to_thenable<T>( make_promise( std::forward<Functor>( fn ), policy ));
+        return to_thenable( make_promise<T>( std::forward<Functor>( fn ), policy ));
     };
 
     //////////
+
+    /*
+     * timeout2 is a variation of timeout that returns a ThenableFuture instead of std::future
+     * */
 
     template <typename T, typename _Rep, typename _Period>
     ThenableFuture<std::decay_t<T>> timeout2( std::chrono::duration<_Rep, _Period> duration, T &&value ) {
